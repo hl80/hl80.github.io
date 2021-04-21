662e700382ec9c55d52fccfc5b2ecf4bca661b19
hl80 <sanjay.vijayr@gmail.com>
2021-04-21 11:22:17 +0530

    added gameover screen

--- src/init.js
+++ src/init.js
@@ -22,6 +22,8 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
+var screenWidth = 0;
+var screenHeight = 0;
 
 function preload() {
   this.load.image('boat', 'assets/boat3.png');
@@ -33,6 +35,8 @@ function preload() {
   this.load.image('down', 'assets/down.png');
   this.load.image('fish', 'assets/fish2.png');
   this.load.image('shark', 'assets/shark2.png');
+  this.load.image('gameover', 'assets/gameover.png');
+  this.load.image('restart', 'assets/restart.png');
 }
 
 function initBubbles(scene, interval, speed, lifetime, deadLine, width, height) {
@@ -75,6 +79,8 @@ function initBubbles(scene, interval, speed, lifetime, deadLine, width, height) 
   return update;
 }
 
+var running = true;
+
 function initFishes(scene, interval, speed, deadLine, width, height, hook, addScore, sharkProb) {
   var fishes = [];
   var sharks = [];
@@ -114,10 +120,15 @@ function initFishes(scene, interval, speed, deadLine, width, height, hook, addSc
 	  'dead' : false,
 	}
 	fish['image'].setOrigin(0, 0);
-	scene.physics.add.overlap(hook, fish['image'], function() { fish['dead'] = true; addScore();})
+	scene.physics.add.overlap(hook, fish['image'], function() { 
+	  if(! fish['dead']) {
+	    fish['dead'] = true;
+	    addScore();
+	  }
+	});
 	//bubble['image'].displayWidth = 75;
 	//bubble['image'].displayHeight = 75;
-	fishes.push(fish)
+	fishes.push(fish);
       }
       else {
 	var y = Phaser.Math.RND.frac();
@@ -131,7 +142,22 @@ function initFishes(scene, interval, speed, deadLine, width, height, hook, addSc
 	  'angspeed' : 180,
 	}
 	shark['image'].setOrigin(0, 0);
-	scene.physics.add.overlap(hook, shark['image'], function() {scene.scene.restart();});
+	scene.physics.add.overlap(hook, shark['image'], function() {
+	  if(running) {
+	    running = false;
+	    var gameover = scene.add.image(screenWidth/2, screenHeight/2, 'gameover');
+	    gameover.displayWidth = screenHeight;
+	    gameover.displayHeight = screenHeight;
+	    var restart = scene.add.image(screenWidth/2, screenHeight*0.8, 'restart');
+	    restart.displayWidth = 200;
+	    restart.displayHeight = 200;
+	    restart.setInteractive();
+	    restart.on('pointerdown', function() {
+	      running = true;
+	      scene.scene.restart();
+	    });
+	  }
+	});
 	//bubble['image'].displayWidth = 75;
 	//bubble['image'].displayHeight = 75;
 	sharks.push(shark);
@@ -150,6 +176,8 @@ var fishFunc;
 function create() {
   var width = this.cameras.main.displayWidth;
   var height = this.cameras.main.displayHeight;
+  screenWidth = width;
+  screenHeight = height;
 
   var data = {
     'score' : 0,
@@ -193,31 +221,41 @@ function create() {
   upButton.setOrigin(0, 0);
   upButton.x = width - upButton.width;
   upButton.y = height/2 - (upButton.height + 100);
-  upButton.setInteractive().on('pointerdown', function(pointer) {
-    if(pointer.isDown)
+  //upButton.setInteractive(new Phaser.Geom.Rectangle(0, 0, 1000, 1000), Phaser.Geom.Rectangle.Contains);
+  //scene.input.setHitAreaRectangle([upButton], -100, -100, upButton.width+100, upButton.height+100);
+  //upButton.setInteractive();
+  upButton.setInteractive(new Phaser.Geom.Rectangle(-upButton.width, -(height/2 - (upButton.height + 100)), 2*upButton.width, height/2), Phaser.Geom.Rectangle.Contains);
+  upButton.on('pointerdown', function(pointer) {
+    if(pointer.isDown && running)
       upDown = true;
   });
-  upButton.setInteractive().on('pointerup', function() {
-    upDown = false;
+  upButton.on('pointerup', function() {
+    if(running)
+      upDown = false;
   });
-  upButton.setInteractive().on('pointerout', function() {
-    upDown = false;
+  upButton.on('pointerout', function() {
+    if(running)
+      upDown = false;
   });
+
 
   var downDown = false;
   var downButton = this.add.image(0, 0, 'down');
   downButton.setOrigin(0, 0);
   downButton.x = width - downButton.width;
   downButton.y = height/2 + 100;
-  downButton.setInteractive().on('pointerdown', function(pointer) {
-    if(pointer.isDown)
+  downButton.setInteractive(new Phaser.Geom.Rectangle(-downButton.width, -100, 2*downButton.width, height/2), Phaser.Geom.Rectangle.Contains);
+  downButton.on('pointerdown', function(pointer) {
+    if(pointer.isDown && running)
       downDown = true;
   });
-  downButton.setInteractive().on('pointerout', function() {
-    downDown = false;
+  downButton.on('pointerout', function() {
+    if(running)
+      downDown = false;
   });
-  downButton.setInteractive().on('pointerup', function() {
-    downDown = false;
+  downButton.on('pointerup', function() {
+    if(running)
+      downDown = false;
   });
 
   hookFunc = function (dt) {
@@ -246,8 +284,8 @@ function create() {
 	hookLevel = 0;
 	hookSpeed = 0;
       }
-      if((hookLevel + waterLevel)> height) {
-	hookLevel = height - waterLevel;
+      if((hookLevel + waterLevel + 56)> height) {
+	hookLevel = height - waterLevel - 56;
 	hookSpeed = 0;
       }
     }
@@ -256,7 +294,7 @@ function create() {
     line.height = (hookLevel + waterLevel) - sailorImage.y;
   }
 
-  var score = this.add.text(0, 0, '0', { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' , fontSize: 48,});
+  var score = this.add.text(width - 200, 20, '0', { fixedWidth: 180, fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' , fontSize: 96, align: "right"});
   var addScore = function () {
     data['score'] += 1;
     score.text = data['score'].toString();
@@ -274,6 +312,8 @@ var time = getTime();
 function update() {
   var newTime = getTime();
   var dt = (newTime - time)/1000.0
+  if (!running)
+    dt = 0;
   bubbleFunc(dt);
   hookFunc(dt);
   fishFunc(dt);
